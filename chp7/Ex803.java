3eb19ab9fd3d43249169c50c9dd4de6b902d27f1
20211814김동연 <161808977+dogang2@users.noreply.github.com>
2024-04-09 16:39:39 +0900

    6주차 끝!

---
+++ chp7/Ex803.java
@@ -0,0 +1,30 @@
+public class Ex803 {
+
+	public static void main(String[] args) {
+		Car sf = new Car();
+		EvCar x = new EvCar();
+		
+		// Drive Santa Fe
+		sf.printDetails();
+		
+		for ( int i = 0; i < 6; i++) {
+			sf.drive();
+		}
+		
+		System.out.println("You have " + sf.getTank() + "L of oil left.");
+		System.out.println("You drove " + sf.getKm() + "Km.");
+		
+		// Drive Model X
+		x.printDetails();
+		
+		for ( int i = 0; i < 20; i++) {
+			if (x.getCharge() <= 10)
+				x.charge();
+			x.drive();
+		}
+		
+		System.out.println("You used " + x.getTank() + "L of oil left.");
+		System.out.println("You have " + x.getTank() + "L of oil left.");
+		System.out.println("You drove " + x.getKm() + "Km.");
+	}
+}
